ca2884e1964130afc159ea6c628d8eaca3970421
Yusufthalibb <yusufthalibb@gmail.com>
2024-10-28 22:25:05 +0700

    Portofolio Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        'font-sans1': '700',
+        'font-sans2': '300',
+      },
     },
   },
   plugins: [],
-}
-
+};
